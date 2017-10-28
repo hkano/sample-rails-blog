af1a504c5eb6920a1ab132a64078e37ada9bca33
Hideaki Kano (@hkano) <kano.hideaki@gmail.com>
2017-10-28 22:14:55 +0900

    fix eager_load config.paths

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module SampleRailsBlog
   class Application < Rails::Application
     config.load_defaults 5.1
-    config.paths.add "#{Rails.root}/lib/utils", eager_load: true
+    config.paths.add "#{Rails.root}/lib", eager_load: true
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
   end
