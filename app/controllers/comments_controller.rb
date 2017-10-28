af19e4bad2e13394d39b80a64623dd737aa5fdc8
Hideaki Kano (@hkano) <kano.hideaki@gmail.com>
2017-10-28 23:07:23 +0900

    fix CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,13 @@ class CommentsController < ApplicationController
   before_action :set_article, only: [:create, :destroy]
 
   def create
-    @comment = @article.comments.create comment_params
+    @comment = @article.comments.create! comment_params
     redirect_to article_path @article
   end
 
   def destroy
     @comment = @article.comments.find params[:id]
-    @comment.destroy
+    @comment.destroy!
     redirect_to article_path @article
   end
 
